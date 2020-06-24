07bc3f3c98b5583ca7eb4948297154dab93c1d98
chipres <ana.chipres16@tectijuana.edu.mx>
2020-06-23 19:19:51 -0700

    mddf

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/sw-utils.js');
 
 
 
-const STATIC_CACHE    = 'static-v7';
+const STATIC_CACHE    = 'static-v8';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
